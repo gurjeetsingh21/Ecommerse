6f135d8d3a41be8db9071a6666fe27e0aed93744
gujeetsingh21 <gujeetsingh2112@gmail.com>
2020-08-17 11:17:22 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+// app
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// routes middlewares
+app.use("/api", userRoutes);
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected"));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
